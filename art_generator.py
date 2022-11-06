79bc67aa99ceee2108e544621cc0a08b188fa9b0
SamTF <elseban88@gmail.com>
2022-11-06 16:36:12 +0100

    added option of saving image to disk

--- art_generator.py
+++ art_generator.py
@@ -12,7 +12,7 @@ with open('.api_key', 'r') as f:
 
 ai = client.StabilityInference(key=api_key, verbose=True)
 
-def generate_img(prompt: str, height:int=512, width:int=512) -> io.BytesIO:
+def generate_img(prompt: str, save_to_disk = True, height:int=512, width:int=512) -> io.BytesIO:
     '''
     Generates an image based on a text prompt using Stable Diffusion.
     Returns bytes-like object.
@@ -23,6 +23,8 @@ def generate_img(prompt: str, height:int=512, width:int=512) -> io.BytesIO:
         width=width
     )
 
+    print(f'[ART_GENERATOR] >>> generating _{prompt}_')
+
     for resp in answers:
         for artifact in resp.artifacts:
             if artifact.finish_reason == generation.FILTER:
@@ -30,11 +32,19 @@ def generate_img(prompt: str, height:int=512, width:int=512) -> io.BytesIO:
                     "Your request activated the API's safety filters and could not be processed."
                     "Please modify the prompt and try again.")
             if artifact.type == generation.ARTIFACT_IMAGE:
-                img = Image.open(io.BytesIO(artifact.binary))
-                # img.save('test3.png')
-                
-                generated_img = io.BytesIO()
-                img.save(generated_img, format='PNG')
-                generated_img.seek(0)
+                print('[ART_GENERATOR] >>> generation finished!')
 
-                return generated_img
+                img = Image.open(io.BytesIO(artifact.binary))
+
+                if save_to_disk:
+                    img.save(f'{prompt}.png')
+                    print('[ART_GENERATOR] >>> saved image to disk')
+                    return None
+                
+                else:
+                    generated_img = io.BytesIO()
+                    img.save(generated_img, format='PNG')
+
+                    return generated_img
+
+print('[ART_GENERATOR] ___ INITIALISED ___')
